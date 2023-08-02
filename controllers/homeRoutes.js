9330ba4e7acf5739e8e0e3b91212a565718368e9
treesaretall <himatthewhurst@hotmail.com>
2023-08-02 19:45:57 -0400

    added homeRoute functions

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,16 +1,39 @@
                                                                 /* ===================== IMPORTS ====================== */
 const router = require("express").Router();                     /* Import express.Router()                              */
+const { Recipe , IngredientsAll } = require('../../models');
 // const withAuth = require("../utils/auth");                   // TODO: Uncomment when withAuth is needed
 
 
                                                                 /* ====================== ROUTES ====================== */
 router.get("/", async (req, res) => {                           /* Render home.handlebars with main.handlebars layout   */
     try {
-        res.render("home", { layout: "main" })
-    } 
-    catch (err) {
-        res.status(500).json(err);
-    }
+    const recipeData = await Recipe.findAll({
+      include: [
+        {
+          model: IngredientsAll,
+          attributes: [
+            'unit_id',
+            'quantity_id',
+            'ingredient_id'
+          ],
+        },
+      ],
+    });
+
+    const recipes = recipeData.map((recipe) =>
+      recipe.get({ plain: true })
+    );
+
+    res.json(recipes);
+
+    res.render('homepage', {
+      recipes,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 
