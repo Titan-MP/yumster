120ee40756df6a8854b24adc6d1ada35b5ff5c24
treesaretall <himatthewhurst@hotmail.com>
2023-08-02 20:16:34 -0400

    home and recipe route fixes

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,6 @@
 const router = require("express").Router();                     /* Import express.Router()                              */
 const apiRoutes = require("./api");                             /* Import apiRoutes from api/index.js                   */
 const homeRoutes = require("./homeRoutes.js");                  /* Import homeRoutes from homeRoutes.js                 */
-const dashboardRoutes = require("./dashboardRoutes.js")         /* Import dashboardRoutes from dashboardRoutes.js       */
 const loginRoutes = require("./loginRoutes.js")                 /* Import loginRoutes from loginRoutes.js               */
 const signupRoutes = require("./signupRoutes.js")               /* Import signupRoutes from signupRoutes.js             */
 const recipeRoutes = require("./recipeRoutes.js")               /* Import recipeRoutes from recipeRoutes.js             */
@@ -11,7 +10,6 @@ const recipeRoutes = require("./recipeRoutes.js")               /* Import recipe
                                                                 /* ====================== ROUTES ====================== */
 router.use("/api", apiRoutes);                                  /* Use apiRoutes from api/index.js                      */
 router.use("/", homeRoutes);                                    /* Use homeRoutes from homeRoutes.js                    */
-router.use("/dashboard", dashboardRoutes);                      /* Use dashboardRoutes from dashboardRoutes.js          */
 router.use("/login", loginRoutes);                              /* Use loginRoutes from loginRoutes.js                  */
 router.use("/signup", signupRoutes);                            /* Use signupRoutes from signupRoutes.js                */
 router.use("/recipe", recipeRoutes);                            /* Use recipeRoutes from recipeRoutes.js                */
