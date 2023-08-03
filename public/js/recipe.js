c3b8e7fb5e0329d6124985ae60595883a390a2f6
ricguer <rj.guerra@outlook.com>
2023-08-03 17:00:16 -0400

    Link Recipe JS Script and Recipe Handlebars

--- public/js/recipe.js
+++ public/js/recipe.js
@@ -1,14 +1,16 @@
-recipeBtn.onclick = event => {
-    event.preventDefault();
-    const recipeId = event.target.dataset.id
+document
+    .getElementById('recipeBtn')
+    .addEventListener('click', event => {
+        event.preventDefault();
+        const recipeId = event.target.dataset.id
 
-    fetch('/recipe/'+ recipeId, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    })
-        .then(response => response.json()).then(info => {
-            alert(info);
+        fetch('/recipe/' + recipeId, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+            .then(response => response.json()).then(info => {
+                alert(info);
+            });
         });
-};
