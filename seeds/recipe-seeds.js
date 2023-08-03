b88695176e49d2e6289b211b290c2d52cf27bd3a
ricguer <rj.guerra@outlook.com>
2023-08-03 08:53:20 -0400

    Add Section Divisions Between Recipe Categories in Home Page

--- seeds/recipe-seeds.js
+++ seeds/recipe-seeds.js
@@ -13,7 +13,7 @@ const recipeData = [
     {
         id: 2,
         recipe_name: 'Caramel Cream Cheese Cinnamon Rolls',
-        description: 'Light and fluffy cinnamon rolls perfect for any occasion! These cinnamon rolls are taken to the next level with a brown butter and caramel cream cheese frosting. This recipe takes about 2.5 hours from start to finish and will make 12 cinnamon rolls',
+        description: 'Light and fluffy cinnamon rolls perfect for any occasion! These cinnamon rolls are taken to the next level with a brown butter and caramel cream cheese frosting. This recipe takes about 2.5 hours from start to finish and will make 12 cinnamon rolls.',
         instructions: '1. using a stand mixer with a dough hook attachment combine the milk, yeast, and brown sugar. Let the mixture sit for 5 - 10 minutes until the mixture becomes bubbly. \n2. Add in the butter, eggs, vanilla, salt, and 3.5 - 4 cups of flour and mix till fully incoprperated (if the dough is still sticky add in more flour till the dough is smooth to the touch.\n3. Cover the bowl with plastic wrap and let it sit till the dough doubles in size (about 1 hour).\n4. While the dough rises prep the filling by combining the brown sugar, sugar, and cinnamon\n5. When the dough is done rising roll it out onto a floured surface unitl you get a 12 x 18 inch rectangle\n6. Spread the butter out onto the dough and sprinkle on the cinnamon sugar mixture.\n7. Starting with the longer end roll the dough into a log keeping it as tigh as possible as you roll. Cut the log into 12 equal sections and place into a greased 9x13 inch baking dish.\n8. Cover the cinnamon rolls in plastic wrap and allow to rise for another 15-20 minutes. Meanwhile preheat the over to 350 F. \n9. Bake the rolls for 25-30 minutes until golden brown.\n10. While the cinnamon rolls cook make the icing. Add the butter to a pot and heat over mediume heat unitl butter is slightly brown and has a nutty smell (about 3 minutes), then remove from heat.\n11. In a seperat bowl beat the cream cheese until fluffy, once fluffy add the brown butter and beat to combine. Then add the powdered sugar, caramel, vanilla, and a pinch of salt.\n12. When the cinnamon rolls come out of the oven top with icing and serve!\nMakes 12 Cinnamon Rolls',
         source: 'Half-Baked Harvest',
         category_id: 1,
@@ -22,7 +22,7 @@ const recipeData = [
     {
         id: 3,
         recipe_name: 'Garlic Butter Chicken and Zucchini Orzo',
-        description: 'A quick and easy dinner to satisfy the whole family. This meal takes one hour to make from start to finish and can server a family of 6',
+        description: 'A quick and easy dinner to satisfy the whole family. This meal takes one hour to make from start to finish and can server a family of 6.',
         instructions: '1. Preheat oven to 400° F\n2. In a medium bowl stir the chicken with 2 tbsp olive oil, 2 tbsp Italian seasoning, Dijon mustard, honey, and a pinch of salt and pepper\n3. Heat 1 tbsp of olive oil in a large skillet over medium-high heat. Add the chicken. Sear on both sides until golden, 3-5 minutes. Remove everything from the skillet and set aside.\n4. To the skillet, add 2 tbsp butter, the orzo, shallots, garlic, and 2 tbsp thyme. Mix in the zucchini slices. Cook until the orzo is toasted (about 2 minutes)\n5.  Add the wine and 2 cups water. Season with salt and pepper. Bring to a boil over high heat. Slide the chicken and any juices left on the plate back into the skillet. Arrange the cheese around the chicken. Bake, uncovered for 15 minutes or until the chicken is cooked through.\n6. While the skillet is in the oven begin making the garlic butter: Lightly brown 4 tbsp butter in a small skillet. Add 1 tbsp of Italian seasoning and 1 tbsp of thyme. Season with salt, pepper, and chili flakes.\n7. Once the chicken comes out of the oven top with the garlic butter and serve!',
         source: 'Half-Baked Harvest',
         category_id: 3,
@@ -49,7 +49,7 @@ const recipeData = [
     {
         id: 6,
         recipe_name: 'Fluffy Cinnamon Pancakes',
-        description: 'Perfect Sunday morning vegan breakfast ready in 30 minutes',
+        description: 'Perfect Sunday morning vegan breakfast ready in 30 minutes.',
         instructions: '1. In a medium bowl mix together water and maple syrup \n2. In a large bowl mix together the dry ingredients \n3. While stirring pour the wet mixture into the dry mixture until fully combined (mixture will be thick) \n4. Heat a non-stick skillet over medium heat \n5. Scoop the batter and spread onto the skillet cook for 2-3 minutes on each side till golden brown\n5. Repeat with the remaining batter\nMakes 4 pancakes',
         source: 'The Foodie Takes Flight',
         category_id: 1,
@@ -85,7 +85,7 @@ const recipeData = [
     {
         id: 10,
         recipe_name: 'Potato Soup with Bacon',
-        description: 'Classic potato soup whipped up in 30 minutes',
+        description: 'Classic potato soup whipped up in 30 minutes.',
         instructions: '1. In a stockpot over low heat cook the bacon pieces till crispy (7-10 minutes). Once cooked remove bacon and set aside. \n2. In the same pot with about 1 tbsp of bacon grease add onions and flour and cook while stirring over medium heat for 3-4 minutes. \n3. Add in the chicken stock, milk, and potatoes. Cook till potatoes are tender (10-12 minutes). \n4. Stir in heavy cream and Velveeta and cook till cheese is completely melted (taste and season with more salt if needed).\n5. Top with bacon and serve. \nMakes 8 servings',
         source: 'Get on My Plate',
         category_id: 2,
